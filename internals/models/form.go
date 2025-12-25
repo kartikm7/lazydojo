e1c656820ff324d13c8be286026e66efd7d03c68
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-12-26 01:40:37 +0530

    feat: added text input
    - added a text input model, yet to be styled

--- internals/models/form.go
+++ internals/models/form.go
@@ -1,21 +1,39 @@
 package models
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
 
-type formModel struct{}
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type formModel struct {
+	textInput textinput.Model
+}
 
 func InitFormModel() formModel {
-	return formModel{}
+	ti := textinput.New()
+	ti.Placeholder = "What you practicing at the Dojo?"
+	ti.Focus()
+	ti.Width = 50
+	return formModel{ti}
 }
 
 func (m formModel) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m formModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return DefaultBinding(msg, m)
+	var cmd tea.Cmd
+
+	updated, globalcmd := DefaultBinding(msg, m)
+	if assert, ok := updated.(formModel); ok {
+		m = assert
+	}
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, tea.Batch(globalcmd, cmd)
 }
 
 func (m formModel) View() string {
-	return "Form"
+	return fmt.Sprintf("Form\n%s", m.textInput.View())
 }
